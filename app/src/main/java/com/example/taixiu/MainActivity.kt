093446c79e94079579adf38a427b671dc2986aa0
HuyHue <dqh1005@gmail.com>
2023-02-07 23:04:43 +0700

    cmt line 34

--- app/src/main/java/com/example/taixiu/MainActivity.kt
+++ app/src/main/java/com/example/taixiu/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        println(value)
+//        println(value)
         resultImageView.setImageResource(img)
         resultImageView.contentDescription=dice.roll().toString()
 
